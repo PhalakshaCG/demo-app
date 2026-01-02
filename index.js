c0417dc6d73668b07c6f55a8e215ad17eccf4f15
RH <146109310+azure-glades@users.noreply.github.com>
2026-01-02 12:40:16 +0530

    Revert "all your rebase are belong to us"

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.get('/weather', async (req, res) => {
       <p>Temperature: ${weatherData.current.temp_c}°C</p>
           <p>Weather Condition: <img src="${weatherData.current.condition.icon}" alt="Weather Icon" style="height: 5mm;"> ${weatherData.current.condition.text}</p>
       <p>Precipitation: ${weatherData.current.precip_mm} mm</p>
-      <p>Condition: <img src="${weatherData.current.condition.icon}" alt="Weather Icon" style="height: 5mm;"> ${weatherData.current.condition.text}</p>
     `);
   } catch (error) {
     res.status(500).send('Error fetching weather data. Please try again later.');
